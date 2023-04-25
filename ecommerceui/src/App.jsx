f493cbff2aeb17e22c02147bc4249d8540824c13
hmfaaiz <smartlaptopme9@gmail.com>
2023-04-25 23:25:40 +0500

    Routing

--- ecommerceui/src/App.jsx
+++ ecommerceui/src/App.jsx
@@ -5,12 +5,34 @@ import Product from "./pages/Product";
 import Register from "./pages/Register";
 import Login from "./pages/Login";
 import Cart from "./pages/Cart";
-const App=()=> {
-  return  <Home/>;
-  // return  <ProductList/>;
-  // return  <Product/>;
-  // return  <Register/>;
-  // return  <Login/>;
-  return  <Cart/>;
+
+import { BrowserRouter as Router, Route, Routes, Navigate} from "react-router-dom";
+const App = () => {
+  const isLoggedIn=true;
+  return(
+    <Router>
+      <Routes >
+        <Route exact path="/"  element={<Home  />}/>
+      </Routes>
+      <Routes >
+        <Route exact path="/products/:category"  element={<ProductList  />}/>
+      </Routes>
+      <Routes >
+        <Route exact path="/product/:id"  element={<Product  />}/>
+      </Routes>
+      <Routes >
+        <Route exact path="/cart"  element={<Cart  />}/>
+      </Routes>
+      <Routes >
+        <Route exact path="/login"/>
+        {isLoggedIn ? <Navigate to="/" /> : <Login />}
+        
+      </Routes>
+      <Routes >
+        <Route exact path="register"  element={<Register  />}/>
+      </Routes>
+    </Router >
+
+  );
 };
 export default App;
